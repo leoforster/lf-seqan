5a8086d6162fed0b31c459111847c085ea551562
leoforster <leoforster@gmail.com>
2017-03-13 16:54:10 +0100

    some testing

--- xdrop/seqan/extend.cpp
+++ xdrop/seqan/extend.cpp
@@ -8,47 +8,53 @@ using namespace seqan;
 
 int main()
 {
-    Score<int> sc(2, -1, -2);
+  Align<Infix<CharString const>::Type> align;
+  resize(rows(align), 2);
+  int score = 0;
+  
+  String<char> a = "SEEDabXcdXefXXX";
+  String<char> b = "SEEDabYcdefYYYY";
+  
+  int left_a = 0; 
+  int left_b = 0;
+  int length = 4;
+  //int right_a = left_a + length;
+  //int right_b = left_b + length;
+  
+  Seed<Simple> seed1(0, 0, 4);          //left=0; length=4
+  Score<> scoring(1, -1, -1);
 
-    Align<Infix<CharString const>::Type> align;
-    resize(rows(align), 2);
+  assignSource(row(align, 0), infix(a, left_a, left_a + length));
+  assignSource(row(align, 1), infix(b, left_b, left_b + length));
+  score = globalAlignment(align, scoring);
+  
+  std::cout << "Initial alignment (score == " << score << ")\n\n"
+            << align;
+  
+  //extendSeed(seed, a, b, 1, MatchExtend());
+  //extendSeed(seed1, a, b, EXTEND_BOTH, MatchExtend());
+  //extendSeed(seed1, a, b, EXTEND_RIGHT, MatchExtend());
+  //extendSeed(seed1, a, b, EXTEND_BOTH, scoring, 2, UnGappedXDrop());
+  extendSeed(seed1, a, b, EXTEND_BOTH, scoring, 2, GappedXDrop());
+  
+  length = endPositionH(seed1) - left_a;
+  assignSource(row(align, 0), infix(a, left_a, left_a + length));
+  assignSource(row(align, 1), infix(b, left_b, left_b + length));
+  score = globalAlignment(align, scoring);
 
-    // We create the following initial situation with subject/query and the
-    // infixes thereof.
-    //
-    //                         infixes/seeds
-    //                             <-->
-    // subject  NNNNNNNNNNTTCCGGGACGGTACACACACGGGGGGGGGG
-    // query               CTCGGGACGGTACAGGCACGGTTTTTTTT
-    CharString subject = "NNNNNNNNNNTTCCGGGACGGTACACACACGGGGGGGGGG";
-    CharString query   = "CTCGGGACGGTACAGGCACGGTTTTTTTT";
-    assignSource(row(align, 0), infix(subject, 19, 23));
-    assignSource(row(align, 1), infix(query, 8, 12));
-    int score = globalAlignment(align, sc);
+  std::cout << "Resulting alignment (score == " << score << ")\n\n"
+            << align;
 
-    std::cout << "Initial alignment of infixes (score == " << score << ")\n\n"
-              << align;
+  //Seed<Simple> seed2(0, 0, 4);          //left=0; length=4
+  //Score<> scoring(1, -1, -1);
+  //extendSeed(seed2, a, b, EXTEND_BOTH, scoring, 2, UnGappedXDrop());
+  //std::cout << "endPositionH(seed2) = " << endPositionH(seed2) << std::endl;
+  //std::cout << "endPositionV(seed2) = " << endPositionV(seed2) << std::endl;
 
-    // The alignment starts at diagonal (23 - 19) = 4.  A band of 4 in each direction has
-    // the following diagonals.
-    int lDiag = 0, uDiag = 8;
-    // Set the x-Drop value to 5.
-    int xDrop = 5;
-    Tuple<unsigned, 4> positions = { {19u, 8u, 23u, 12u} };
-    score = extendAlignment(align, score, subject, query, positions, EXTEND_BOTH,
-                            lDiag, uDiag, xDrop, sc);
+  //Seed<Simple> seed3(0, 0, 4);          //left=0; length=4
+  //extendSeed(seed3, a, b, EXTEND_BOTH, scoring, 2, GappedXDrop());
+  //std::cout << "endPositionH(seed3) = " << endPositionH(seed3) << std::endl;
+  //std::cout << "endPositionV(seed3) = " << endPositionV(seed3) << std::endl;
 
-    std::cout << "Resulting alignment (score == " << score << ")\n\n"
-              << align;
-
-    std::cout << "source(row(align, 0)) == " << source(row(align, 0)) << " (full sequence)\n"
-              << "source(row(align, 1)) == " << source(row(align, 1)) << " (full sequence)\n"
-              << "\n"
-              << "clipping positions of row 0: " << clippedBeginPosition(row(align, 0))
-              << ", " << clippedEndPosition(row(align, 0)) << "\n"
-              << "clipping positions of row 1: " << clippedBeginPosition(row(align, 1))
-              << ", " << clippedEndPosition(row(align, 1)) << "\n";
-
-
-    return 0;
+  return 0;
 }
