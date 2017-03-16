32697945b1b8124842120e9ffa9d4b4e4786ef85
leoforster <leoforster@gmail.com>
2017-03-16 23:12:42 +0100

    update python script to call seqan and compare matches based on some parameters

--- xdrop/compare.py
+++ xdrop/compare.py
@@ -2,10 +2,17 @@ import sys
 import os
 import subprocess
 import argparse
+from Bio import SeqIO
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", help="input fasta file", type=str)
+parser.add_argument("-s", help="seedlength", type=int)
 args = parser.parse_args()
+
+if args.s:
+  seedlen = args.s
+else:
+  seedlen = 8
 
 if args.i != None:
   if args.i[0] == "/":
@@ -16,7 +23,11 @@ if args.i != None:
   if not os.path.isfile(infile): #and not os.path.isfile(infile + ".esq"):
     print "fail %s" %infile
     sys.exit()
+else:
+  print "fail %s" %infile
+  sys.exit()
 
+#encseq encode places encoded files in script calling directory -- change it?
 try:
   subprocess.call(["gt encseq encode %s" %infile], shell=True, stderr=subprocess.STDOUT)
 except:
@@ -25,7 +36,8 @@ except:
 
 #gt seed_extend -ii longseeded1 -outfmt seed failed_seed -maxmat 2 -l 100 -extendxdrop -seedlength 20
 try:
-  out = subprocess.check_output(["gt seed_extend -ii %s -seedlength 8 -outfmt seed failed_seed" %infile], shell=True, stderr=subprocess.STDOUT)
+  seed_out = subprocess.check_output(["gt seed_extend -ii %s -seedlength %i -outfmt seed failed_seed" %(infile, seedlen)], shell=True, stderr=subprocess.STDOUT)
+  seed_out = seed_out.split("\n")
 except:
   print "failed to get seeds"
   sys.exit()
@@ -43,7 +55,7 @@ def makeseed(sid, qid, seed):
 #q.len, q.seqnum, q.start, score, editdist, identity, 
 #seedlen, s.seedstart, q.seedstart
 seedlist = []
-for seed in out.split("\n"):
+for seed in seed_out:
   if seed != "":
     if seed.startswith("#"):
       if seed.startswith("# failed_seed"):
@@ -58,15 +70,83 @@ for seed in out.split("\n"):
       seedlist.append(makeseed(seed[1], seed[5], seed[10:]))
 
 #print len(seedlist)
-outfile = "seeds.txt"
-with open(outfile, "w") as f:
-  for seed in seedlist:
-    f.write(seed[0][0] + ", " + seed[0][1] + ", " + 
-            seed[1][0] + ", " + seed[1][1] + ", " + 
-            seed[2] + "\n")
+#outfile = "seeds.txt"
+#with open(outfile, "w") as f:
+  #for seed in seedlist:
+    #f.write(seed[0][0] + ", " + seed[0][1] + ", " + 
+            #seed[1][0] + ", " + seed[1][1] + ", " + 
+            #seed[2] + "\n")
+  
+fasta_sequences = SeqIO.parse(open(infile),'fasta')
+  
+seqan_path = "~/Desktop/projects/hiwi/lf-seqan/xdrop/seqan/extend.x"
+seqlist = []; count = 0
+for fasta in fasta_sequences:
+  count += 1
+  seqlist.append(str(fasta.seq))
+
+seqan_path += " %i" %count
+seqan_path += " %i" %len(seedlist)
+
+for seq in seqlist:
+  seqan_path += " " + seq
+  
+for seed in seedlist:
+  seqan_path += " " + seed[0][0] + " " + seed[1][0] + " " + seed[0][1] + " " + seed[1][1] + " " + seed[2]
 
 try:
-  out = subprocess.check_output(["~/Desktop/projects/hiwi/lf-seqan/xdrop/seqan/test.x"], shell=True, stderr=subprocess.STDOUT)
+  extend_out = subprocess.check_output([seqan_path], shell=True, stderr=subprocess.STDOUT)
+  extend_out = extend_out.split("\n")
 except:
   print "failed to extend seeds"
   sys.exit()
+
+def parts(match):
+  try:
+    [slen, sid, spos, strand, qlen, qid, qpos, score, edist, ident] = match.split(" ")[:10]
+    return [slen, sid, spos, strand, qlen, qid, qpos, score, edist, ident]
+  except:
+    return None
+  
+def compare_match(seqan, gt):
+  p1 = parts(seqan)
+  p2 = parts(gt)
+  
+  if p1 == None or p2 == None:
+    return False
+  
+  for i in range(len(p1)):
+    if i in [3, 7, 8, 9]: #skip unsure lines
+      continue
+    if p1[i] != p2[i]:
+      return False
+  return True
+
+#integrate above?
+cand = []
+for seed in seed_out:
+  if seed != "":
+    if not seed.startswith("#"):
+      cand.append(seed)
+
+matches = []
+for s in extend_out:
+  for c in cand:
+    if compare_match(s, c):
+      matches.append(s)
+      
+print matches
+
+
+
+
+
+
+
+
+
+
+
+
+
+
